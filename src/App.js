2dd19a2a27719a39f5a77470d58aa33889719193
Bhavesh Varu <bhavesh.varu97@gmail.com>
2022-04-22 18:41:00 +0530

    added delete func

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
-import './App.css';
+import CreateArea from './components/CreateArea'
 import Header from './components/Header'
+import React, { useState } from 'react'
+import Note from './components/Note'
+
 function App() {
+  const [notes, setNotes] = useState([])
+  function addNote(newNote) {
+    setNotes((prevNotes) => {
+      return [...prevNotes, newNote]
+    })
+  }
+  function deleteNote(noteToDelete) {
+    setNotes((prevNotes) => {
+      return notes.splice(prevNotes, 1)
+    })
+  }
   return (
-    <div className="App">
-    <Header/>
-     
+    <div className='App'>
+      <Header />
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        )
+      })}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/CreateArea.js
+++ src/components/CreateArea.js
@@ -1,7 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Fab from '@material-ui/core/Fab'
+import AddIcon from '@material-ui/icons/Add'
+const CreateArea = (props) => {
+    const [note, setNote] = useState({
+        title: "",
+        content: ""
+      });
+    
+      function handleChange(event) {
+        const { name, value } = event.target;
+    
+        setNote(prevNote => {
+          return {
+            ...prevNote,
+            [name]: value
+          };
+        });
+      }
+    
+      function submitNote(event) {
+        props.onAdd(note);
+        setNote({
+          title: "",
+          content: ""
+        });
+        event.preventDefault();
+      }
+  return (
+    <>
+      <form className='create-note'>
 
-const CreateArea = () => {
-  return <div>CreateArea</div>
+          <input
+            name="title"
+            onChange={handleChange}
+            value={note.title}
+            placeholder="Title"
+          />
+
+        <textarea
+          name="content"
+          onChange={handleChange}
+          value={note.content}
+          placeholder="Take a note..."
+          rows={3}
+        />
+        <Fab onClick={submitNote}>
+          <AddIcon />
+        </Fab>
+      </form>
+    </>
+  )
 }
 
 export default CreateArea
