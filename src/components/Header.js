ae7b331db33035d79aba704de1c7cf23f57f4923
Bhavesh Varu <bhavesh.varu97@gmail.com>
2022-04-22 16:13:49 +0530

    initialize repo and added header component

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import HighLightIcon from '@material-ui/icons/Highlight'
+
+const Header = () => {
+  return (
+    <header>
+      <h1>
+        <HighLightIcon />
+        To Do List
+      </h1>
+    </header>
+  )
+}
+
+export default Header
